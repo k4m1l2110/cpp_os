77217b584bd41f22a8c07038ac80e44ab4b81064
kamil <ghost150503@gmail.com>
2023-11-03 17:13:23 +0100

    Defined types helper

---
+++ types.h
@@ -0,0 +1,20 @@
+//
+// Created by kamil on 30.10.23.
+//
+
+#ifndef OS_TEST_TYPES_H
+#define OS_TEST_TYPES_H
+
+typedef char int8_t;
+typedef unsigned char uint8_t;
+
+typedef short int16_t;
+typedef unsigned short uint16_t;
+
+typedef int int32_t;
+typedef unsigned int uint32_t;
+
+typedef long long int int64_t;
+typedef unsigned long long int uint64_t;
+
+#endif //OS_TEST_TYPES_H
