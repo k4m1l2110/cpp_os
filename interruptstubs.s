53735f26c26d4ad1fd5f52f5b856a89d1cc5819a
kamil <ghost150503@gmail.com>
2023-11-05 23:47:36 +0100

    Interrupts

--- interruptstubs.s
+++ interruptstubs.s
@@ -1,55 +1,61 @@
 .set IRQ_BASE, 0x20
 
-.section .text
+hello:
+    .string "Interrupt handled!\n"
 
+
+.section .text
+.extern Print
 .extern _ZN16InterruptManager15HandleInterruptEhj
 
 .macro HandleException num
 .global _ZN16InterruptManager15HandleException\num\()Ev
 _ZN16InterruptManager15HandleException\num\()Ev:
-    movb $\num, interruptnumber
+    movb $\num, (interruptnumber)
     jmp int_bottom
 .endm
 
 .macro HandleInterruptRequest num
 .global _ZN16InterruptManager26HandleInterruptRequest\num\()Ev
 _ZN16InterruptManager26HandleInterruptRequest\num\()Ev:
-    movb $\num + IRQ_BASE, interruptnumber
+    movb $\num + IRQ_BASE, (interruptnumber)
     pushl $0
     jmp int_bottom
 .endm
-
-HandleException 0x00
-HandleException 0x01
 
 HandleInterruptRequest 0x00
 HandleInterruptRequest 0x01
 
 int_bottom:
-    pusha
-    pushl %ds
-    pushl %es
-    pushl %fs
-    pushl %gs
+    pushl %ebp
+	pushl %edi
+	pushl %esi
 
-    pushl %esp
+	pushl %edx
+	pushl %ecx
+	pushl %ebx
+	pushl %eax
+
+	pushl %esp
     push (interruptnumber)
     call _ZN16InterruptManager15HandleInterruptEhj
     # addl $5, %esp
     movl %eax, %esp
 
-    popl %gs
-    popl %fs
-    popl %es
-    popl %ds
-    popa
+    popl %eax
+	popl %ebx
+	popl %ecx
+	popl %edx
+
+	popl %esi
+	popl %edi
+	popl %ebp
+
+	add $4, %esp
 
 .global _ZN16InterruptManager22IgnoreInterruptRequestEv
 _ZN16InterruptManager22IgnoreInterruptRequestEv:
     iret
-
-_stop:
-    cli
 
 .data
     interruptnumber: .byte 0
